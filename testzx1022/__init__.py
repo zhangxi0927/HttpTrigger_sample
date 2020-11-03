e2b5e0e8dc6a78b41d2151d0df40a8ca987fdc13
zhangxi0927 <56713945+zhangxi0927@users.noreply.github.com>
2020-11-04 04:37:37 +0900

    Update __init__.py

--- testzx1022/__init__.py
+++ testzx1022/__init__.py
@@ -1,3 +1,30 @@
+# import logging
+
+# import azure.functions as func
+
+
+# def main(req: func.HttpRequest) -> func.HttpResponse:
+#     logging.info('Python HTTP trigger function processed a request.')
+
+#     name = req.params.get('name')
+#     if not name:
+#         try:
+#             req_body = req.get_json()
+#         except ValueError:
+#             pass
+#         else:
+#             name = req_body.get('name')
+
+#     if name:
+#         return func.HttpResponse(f"Hello, {name}sann. This HTTP triggered function executed successfully.")
+#     else:
+#         return func.HttpResponse(
+#              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+#              status_code=200
+#         )
+
+
+
 import logging
 
 import azure.functions as func
@@ -6,17 +33,20 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
+    first_name = req.params.get('first_name')
+    last_name = req.params.get('last_name')
+    if not (first_name and last_name):
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            first_name = req_body.get('first_name')
+            last_name = req_body.get('last_name')
+            
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}sann. This HTTP triggered function executed successfully.")
+    if first_name and last_name:
+        return func.HttpResponse(f"こんにちは、{last_name}  {first_name}さん")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
